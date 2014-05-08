3deb48967e985fcf875102d257bafe51c37ad236
aleksandar-mitrevski <aleksandar.mitrevski.1990@gmail.com>
2014-05-09 00:24:29 +0200

    Adding an approximate inference engine that performs rejection sampling; a test case is updated as well

---
+++ inference/approximate_inference.py
@@ -0,0 +1,113 @@
+import numpy
+
+class ApproximateInferenceEngine(object):
+    def __init__(self, network):
+        """Defines an engine for performing approximate inference in a discrete Bayesian network.
+
+        Keyword arguments:
+        network -- A 'libpgm.discretebayesiannetwork' object representing a discrete Bayesian network.
+
+        """
+        self.network = network
+
+        #a topological order will be needed in the process of generating variable assignments
+        self.network.toporder()
+
+    def perform_inference(self, query_variable, evidence_variables, number_of_samples):
+        """Calculates the probability distribution P(query_variable|evidence_variables).
+        Assumes that we have only one query variable.
+
+        Keyword arguments:
+        query_variable -- The name of the query variable (as called in the network).
+        evidence_variables -- A dictionary containing variable names as keys and observed values as values.
+        number_of_samples -- The number of samples that should be used in the sampling process.
+
+        Returns:
+        distribution -- A dictionary containing the values of the query variable as keys and the probabilities as values.
+
+        """
+        evidence_supporting_sample_counter = 0.
+        distribution = dict()
+        number_of_variable_values = len(self.network.Vdata[query_variable]['vals'])
+        for i in xrange(number_of_variable_values):
+            distribution[self.network.Vdata[query_variable]['vals'][i]] = 0.
+
+        for i in xrange(number_of_samples):
+            sample_assignments = self.generate_sample()
+            supports_evidence = True
+            for key in evidence_variables.keys():
+                if evidence_variables[key] != sample_assignments[key]:
+                    supports_evidence = False
+                    break
+
+            if supports_evidence:
+                evidence_supporting_sample_counter = evidence_supporting_sample_counter + 1.
+                distribution[sample_assignments[query_variable]] = distribution[sample_assignments[query_variable]] + 1.
+
+        if evidence_supporting_sample_counter > 1e-10:
+            for key in distribution.keys():
+                distribution[key] = distribution[key] / evidence_supporting_sample_counter
+        return distribution
+
+    def generate_sample(self):
+        """Generates a random assignment for the variables in the network.
+        The assignment respects the given conditional probabilities in the network.
+
+        Returns:
+        assigned_values -- A dictionary containing variable names and their assigned values.
+
+        """
+        number_of_variables = len(self.network.V)
+        assigned_values = dict()
+        for i in xrange(number_of_variables):
+            assigned_values[self.network.V[i]] = ''
+
+        for i in xrange(number_of_variables):
+            current_variable = self.network.V[i]
+            parent_values = self.get_parent_values(current_variable, assigned_values)
+
+            cumulative_distribution = [0.]
+            if parent_values == None:
+                number_of_values = len(self.network.Vdata[current_variable]['cprob'])
+                for i in xrange(number_of_values):
+                    cumulative_distribution.append(cumulative_distribution[i] + self.network.Vdata[current_variable]['cprob'][i])
+            else:
+                number_of_values = len(self.network.Vdata[current_variable]['cprob'][parent_values])
+                for i in xrange(number_of_values):
+                    cumulative_distribution.append(cumulative_distribution[i] + self.network.Vdata[current_variable]['cprob'][parent_values][i])
+
+            value_index = 1
+            number_of_values = len(cumulative_distribution)
+            random_number = numpy.random.rand()
+            while value_index < number_of_values and random_number > cumulative_distribution[value_index]:
+                value_index = value_index + 1
+
+            #we decrease the index by 1 because of the additional value in the the cumulative distribution array
+            value_index = value_index - 1
+            assigned_values[current_variable] = self.network.Vdata[current_variable]['vals'][value_index]
+
+        return assigned_values
+
+    def get_parent_values(self, variable, assigned_values):
+        """Returns the assigned values to the parent variables of a given variable.
+
+        Keyword arguments:
+        variable -- A string representing a variable in the network.
+        assigned_values -- A dictionary containing value assignments to variables.
+
+        Returns:
+        parent_values_string -- A string representing the values assigned to the parents of the given variable.
+        The string is in a format compatible with the network representation.
+
+        """
+        if self.network.Vdata[variable]['parents'] == None:
+            return None
+        else:
+            number_of_parents = len(self.network.Vdata[variable]['parents'])
+            parent_values = []
+            for i in xrange(number_of_parents):
+                current_parent = self.network.Vdata[variable]['parents'][i]
+                parent_values.append(assigned_values[current_parent])
+            parent_values_string = "[" + ", ".join("'" + x + "'" for x in parent_values) + "]"
+
+        return parent_values_string
--- inference_test.py
+++ inference_test.py
@@ -3,6 +3,7 @@ from libpgm.graphskeleton import GraphSkeleton
 from libpgm.discretebayesiannetwork import DiscreteBayesianNetwork
 
 from inference.exact_inference import ExactInferenceEngine
+from inference.approximate_inference import ApproximateInferenceEngine
 
 node_data = NodeData()
 network_skeleton = GraphSkeleton()
@@ -10,13 +11,19 @@ node_data.load('network.txt')
 network_skeleton.load('network.txt')
 network = DiscreteBayesianNetwork(network_skeleton, node_data)
 
-inference_engine = ExactInferenceEngine(network)
+exact_inference_engine = ExactInferenceEngine(network)
+approximate_inference_engine = ApproximateInferenceEngine(network)
+
 query_variable = 'Burglary'
 evidence_variables = {'MaryCalls': 'true', 'JohnCalls': 'true'}
-resulting_distribution = inference_engine.perform_inference(query_variable, evidence_variables)
-print resulting_distribution
+resulting_distribution = exact_inference_engine.perform_inference(query_variable, evidence_variables)
+print 'P(B|m,j) - exact: ', resulting_distribution
+resulting_distribution = approximate_inference_engine.perform_inference(query_variable, evidence_variables, 100000)
+print 'P(B|m,j) - approximate: ', resulting_distribution
 
 query_variable = 'JohnCalls'
 evidence_variables = {'MaryCalls': 'true'}
-resulting_distribution = inference_engine.perform_inference(query_variable, evidence_variables)
-print resulting_distribution
+resulting_distribution = exact_inference_engine.perform_inference(query_variable, evidence_variables)
+print 'P(j|m) - exact: ', resulting_distribution
+resulting_distribution = approximate_inference_engine.perform_inference(query_variable, evidence_variables, 100000)
+print 'P(j|m) - approximate: ', resulting_distribution
